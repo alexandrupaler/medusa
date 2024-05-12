89b12fbca225edd93ae47c461862b11cada91f7c
karoliina.i.oksanen@gmail.com <karoliina.i.oksanen@gmail.com>
2024-05-12 14:53:17 +0300

    result pngs

--- main.py
+++ main.py
@@ -6,13 +6,32 @@ import stimcirq
 
 if __name__ == '__main__':
 
+    # todo:
+    # - fix states being on wrong qubits with adder! (check if f in name?)
+    # - run simulation
+    # - check if there are any bugs
+    # - prepare 3d plot: 
+        # - error 1% and 10%
+        # - set number of flags
+        # - adder size, 2-5
+    # - prepare & send off to triton
+
+    # bugs:
+    # - map heuristic has a bug: doesn't work with the adder circuit
+
+
+    adder2 = test_circuits.adder(2)
+    adder2 = test_circuits.change_measurement_and_resets(adder2)
+    print(adder2)
+
     c = compiler.FlagCompiler()
 
     icm_circuit: cirq.Circuit = c.decompose_to_ICM(test_circuits.test_circuit2())
+    #icm_circuit: cirq.Circuit = c.decompose_to_ICM(adder2)
 
     print(icm_circuit)
 
-    f_cir_map = c.add_flag(icm_circuit, strategy="map")
+    #f_cir_map = c.add_flag(icm_circuit, strategy="map")
     f_cir_random = c.add_flag(icm_circuit,number_of_x_flag=3,number_of_z_flag=3)
     f_cir_heuristic = c.add_flag(icm_circuit, strategy="heuristic")
 
@@ -20,20 +39,8 @@ if __name__ == '__main__':
     print("Flags inserted!")
     print("\n")
 
-    """
-    test_circuit: cirq.Circuit = c.decompose_to_ICM(test_circuits.logical_hadamard_1_noflags())
-    icm_circuit = test_circuit
-    flag_circuit = test_circuits.add_flags_to_test_hadamard(test_circuit)
-
-    stimc = stimcirq.cirq_circuit_to_stim_circuit(flag_circuit)
-    print(stimc)
-    """
-
     flag_circuit = f_cir_heuristic
 
-    # print("\n")
-    # print(f_cir)
-    # print("\n")
     
     # plots the logical error rate of flagged and flagless circuit with different inpout states and error rates
     # the noise is randomized depolarising noise
