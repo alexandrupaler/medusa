de078849d468f247a27c2ae81205142e759f49a3
Oksanen Karoliina <karoliina.i.oksanen@gmail.com>
2024-06-17 21:54:50 +0300

    update julia file used with jabalizer

--- icm_converter.jl
+++ icm_converter.jl
@@ -4,11 +4,13 @@ using PythonCall
 cirq = pyimport("cirq");
 gates_to_decomp = ["T", "T^-1"];
 
-icm_input = Jabalizer.load_circuit_from_cirq_json("input_cirq_circuit.json")
-(icm_circuit,_) = Jabalizer.compile(icm_input, 10, gates_to_decomp)
-Jabalizer.save_circuit_to_cirq_json(icm_circuit,"output_cirq_icm_circuit.json");
+function icm_converter_ij(i, j)
+    icm_input = Jabalizer.load_circuit_from_cirq_json("input_cirq_circuit" * string(i) * string(j) * ".json")
+    (icm_circuit,_) = Jabalizer.compile(icm_input, 10, gates_to_decomp)
+    Jabalizer.save_circuit_to_cirq_json(icm_circuit,"output_cirq_icm_circuit" * string(i) * string(j) * ".json");
+end
 
-cirq_circuit = cirq.read_json("output_cirq_icm_circuit.json")
-print(cirq_circuit)
+a = parse(Int64, ARGS[1])
+b = parse(Int64, ARGS[2])
 
-
+icm_converter_ij(a, b)
