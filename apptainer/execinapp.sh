1c258d1c1484442c90fd88ebe87d3613facff1d5
oksanenkaroliina <karoliina.i.oksanen@gmail.com>
2024-11-08 17:17:35 +0000

    wip of apptainer in app runs

---
+++ apptainer/execinapp.sh
@@ -0,0 +1,6 @@
+export PYTHONPATH=$PYTHONPATH:/medusa
+
+cd /medusa
+
+# line to run 
+python3 docker_test_main.py
---
+++ apptainer/run.sh
@@ -0,0 +1,1 @@
+apptainer exec -B medusa:/medusa -e -C images/medusa.sif bash /medusa/execinapp.sh
