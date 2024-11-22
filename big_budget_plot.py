8ac7929616b8b788b1be0e5a2ec681b19d50ab69
oksanenkaroliina <karoliina.i.oksanen@gmail.com>
2024-11-22 23:28:26 +0000

    more big budget result plots

--- big_budget_plot.py
+++ big_budget_plot.py
@@ -22,6 +22,18 @@ if __name__ == '__main__':
         res = pd.read_csv("adder2/fc_" + circuit_type + "_" + circuit_size + "_" + error_mod + "_" + str(error_rate) + ".csv")
         icm_small = pd.read_csv("adder2/icm_small_" + circuit_type + "_" + circuit_size + "_" + error_mod + "_" + str(error_rate) + ".csv")
 
-        plt.scatter(int(circuit_size), res, color='red')
-        plt.scatter(int(circuit_size), icm_small, color='blue')
-    plt.savefig("bigbudget2.png")
+        # uncomment below for flag and icm error plotted together
+        #plt.scatter(int(circuit_size), res, color='red')
+        #plt.scatter(int(circuit_size), icm_small, color='blue')
+        #plt.ylabel("logical error rate")
+
+        # uncomment below for difference between flag and icm error
+        plt.scatter(int(circuit_size), (res - icm_small), color='red')
+        plt.ylabel("logical error rate difference")
+        
+        # uncomment below for flag error mod
+        #plt.scatter(int(circuit_size), error_mod, color='blue')
+        #plt.ylabel("flag error mod")
+
+    plt.xlabel("circuit size")
+    plt.savefig("bigbudget2diff.png")
