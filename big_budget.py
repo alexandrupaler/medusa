e56bb83a45b660393255f76caaf3b11d93318e3e
alexandru paler <alexandrupaler@gmail.com>
2024-12-09 13:27:23 +0200

    some refactoring

--- big_budget.py
+++ big_budget.py
@@ -11,6 +11,9 @@ import numpy as np
 
 from generator_utils import generate_circuits
 
+config = {}
+config["circuits"] = "/circuits/"
+config["logs"] = "/logs/"
 
 if __name__ == '__main__':
 
@@ -20,10 +23,19 @@ if __name__ == '__main__':
     number_of_runs = 10
     error_rate = 0.001
     epsilon_target = 0.005
-    
+
+
     def run_simulation(icm_circuit, flag_circuit, error_mod, error_rate):
-        results, results_icm, _, _, _ = evaluate.stabilizers_robustness_and_logical_error(flag_circuit, icm_circuit, number_of_runs, [error_rate], False, "", "budget" + str(error_mod))
-        return results, results_icm
+        results_flag, results_icm, _, _, _ = \
+            evaluate.stabilizers_robustness_and_logical_error(flag_circuit,
+                                                              icm_circuit,
+                                                              number_of_runs,
+                                                              [error_rate],
+                                                              plotting=False,
+                                                              plot_title="",
+                                                              noise_type=f"budget{error_mod}")
+        return results_flag, results_icm
+
 
     def parallel_simulation(inp):
 
@@ -31,17 +43,12 @@ if __name__ == '__main__':
 
         # fetch circuit files
         # get icm i-1 logical error rate
-        small_icm_fname = "circuits/icm_" + circuit_type + "_" + str(circuit_size-1) + ".json"
-        small_icm = cirq.read_json(small_icm_fname)
-        small_fc_fname = "circuits/fc_" + circuit_type + "_" + str(circuit_size-1) + ".json"
-        small_fc = cirq.read_json(small_fc_fname)
-        
-        # get icm and flag i logical error rate
-        large_icm_fname = "circuits/icm_" + circuit_type + "_" + str(circuit_size) + ".json"
-        large_icm = cirq.read_json(large_icm_fname)
-        large_fc_fname = "circuits/fc_" + circuit_type + "_" + str(circuit_size) + ".json"
-        large_fc = cirq.read_json(large_fc_fname)
+        small_icm = cirq.read_json(f"{config['circuits']}/icm_{circuit_type}_{circuit_size - 1}.json")
+        small_fc = cirq.read_json(f"{config['circuits']}/fc_{circuit_type}_{circuit_size - 1}.json")
 
+        # get icm and flag i logical error rate
+        large_icm = cirq.read_json(f"{config['circuits']}/icm_{circuit_type}_{circuit_size}.json")
+        large_fc = cirq.read_json(f"{config['circuits']}/fc_{circuit_type}_{circuit_size}.json")
 
         # TODO: the i-1 could be done elsewhere
         _, small_icm_failure_rate = run_simulation(small_icm, small_fc, 1, error_rate)
@@ -58,27 +65,31 @@ if __name__ == '__main__':
             diff = small_icm_failure_rate - large_fc_failure_rate
             warnings.warn(str(diff))
 
-            if abs(diff) < epsilon_target: #abs(diff) / res_icm_small < goal:
+            if abs(diff) < epsilon_target:  # abs(diff) / res_icm_small < goal:
                 done = True
             elif diff < 0:
                 er_b = error_mod
             else:
                 er_a = error_mod
-        
+
         # save as csvs
         res_df = pd.DataFrame(large_fc_failure_rate)
+        res_df.to_csv(f"{config['logs']}/fc_{circuit_type}_{circuit_size}_{error_mod}_{error_rate}.csv", index=False)
+
         res_icm_df = pd.DataFrame(large_icm_failure_rate)
+        res_icm_df.to_csv(f"{config['logs']}/icm_{circuit_type}_{circuit_size}_{error_mod}_{error_rate}.csv",
+                          index=False)
+
         res_icm_small_df = pd.DataFrame(small_icm_failure_rate)
-        res_df.to_csv("bigbudget/fc_" + circuit_type + "_" + str(circuit_size) + "_" + str(error_mod) + "_" + str(error_rate) + ".csv",index=False)
-        res_icm_df.to_csv("bigbudget/icm_" + circuit_type + "_" + str(circuit_size) + "_" + str(error_mod) + "_" + str(error_rate) + ".csv",index=False)
-        res_icm_small_df.to_csv("bigbudget/icm_small_" + circuit_type + "_" + str(circuit_size) + "_" + str(error_mod) + "_" + str(error_rate) + ".csv",index=False)
+        res_icm_small_df.to_csv(
+            f"{config['logs']}/icm_small_{circuit_type}_{circuit_size}_{error_mod}_{error_rate}.csv", index=False)
 
 
     # uncomment to generate circuit jsons
     generate_circuits(min_qubits=4, max_qubits=40, number_of_bench_samples=1)
 
-    circuit_sizes = range(5, 40+1)
-    #circuit_types = ["adder", "b1", "b2", "b3"]
+    circuit_sizes = range(5, 40 + 1)
+    # circuit_types = ["adder", "b1", "b2", "b3"]
     circuit_types = ["b1", "b2", "b3"]
 
     paramlist = list(itertools.product(circuit_types, circuit_sizes))
