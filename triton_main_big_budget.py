dc7c7a7b1e1fcfb0b529ce15cd647169ea1fe783
alexandru paler <alexandrupaler@gmail.com>
2024-12-20 14:45:06 +0200

    plot - before test

--- triton_main_big_budget.py
+++ triton_main_big_budget.py
@@ -138,18 +138,18 @@ if __name__ == '__main__':
             json.dump(last_values, report, default=numpy_to_list)
 
         # save as CSVs - single value per file - legacy
-        err_mod_avg = last_values["averages"]["error_mod"]
-        
-        res_df = pd.DataFrame([last_values["averages"]["large_fc_failure_rate"]])
-        res_df.to_csv(f"{config['logs']}fc_{circuit_type}_{circuit_size}_{err_mod_avg}_{error_rate}.csv", index=False)
-        
-        res_icm_df = pd.DataFrame([last_values["averages"]["large_icm_failure_rate"]])
-        res_icm_df.to_csv(f"{config['logs']}icm_{circuit_type}_{circuit_size}_{err_mod_avg}_{error_rate}.csv",
-                          index=False)
-
-        res_icm_small_df = pd.DataFrame([last_values["averages"]["small_icm_failure_rate"]])
-        res_icm_small_df.to_csv(
-            f"{config['logs']}icm_small_{circuit_type}_{circuit_size}_{err_mod_avg}_{error_rate}.csv", index=False)
+        # err_mod_avg = last_values["averages"]["error_mod"]
+        #
+        # res_df = pd.DataFrame([last_values["averages"]["large_fc_failure_rate"]])
+        # res_df.to_csv(f"{config['logs']}fc_{circuit_type}_{circuit_size}_{err_mod_avg}_{error_rate}.csv", index=False)
+        #
+        # res_icm_df = pd.DataFrame([last_values["averages"]["large_icm_failure_rate"]])
+        # res_icm_df.to_csv(f"{config['logs']}icm_{circuit_type}_{circuit_size}_{err_mod_avg}_{error_rate}.csv",
+        #                   index=False)
+        #
+        # res_icm_small_df = pd.DataFrame([last_values["averages"]["small_icm_failure_rate"]])
+        # res_icm_small_df.to_csv(
+        #     f"{config['logs']}icm_small_{circuit_type}_{circuit_size}_{err_mod_avg}_{error_rate}.csv", index=False)
 
 
     # uncomment to generate circuit jsons
--- util_plot.py
+++ util_plot.py
@@ -1,7 +1,8 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import cirq
 import os
+import sys
+import matplotlib.pyplot as plt
+
+import json
 
 # Some notes:
 #
@@ -14,56 +15,86 @@ import os
 
 if __name__ == '__main__':
 
-    path = "importancebudget/"
+    if len(sys.argv) < 3:
+        CRED = '\033[91m'
+        CEND = '\033[0m'
+        print(CRED + "Will run only if provided two parameters:\n\t this_script [path_of_jsons] [plot_file_name]" + CEND)
+        exit(1)
+
+    elif len(sys.argv) == 3:
+        path = sys.argv[1]
+        plot_fname = sys.argv[2]
+
+    # path = "importancebudget/"
     error_rate = 0.001
-    
+
     files = list(filter(lambda f: f.startswith("fc_"), os.listdir(path)))
-    files.sort(key = lambda w: int(w.split("_")[2]))
+    files.sort(key=lambda w: int(w.split("_")[2]))
 
     for filename in files:
 
         parts = filename.split("_")
         circuit_type = parts[1]
         circuit_size = parts[2]
-        error_mod = parts[3]
 
-        #if 'b' in str(circuit_type):
+        # Check the circuit type
+        # if 'b' in str(circuit_type):
         if 'a' in str(circuit_type):
-
             # If you know the circuit type, size and error_rate
             # then you can load the json and read the values from the dictionary
-            # with open(f"{config['logs']}/report_{circuit_type}_{circuit_size}_{error_rate}.json", "r") as report:
-            #     last_values = json.load(report)
+            last_values = {}
+            with open(f"{path}/report_{circuit_type}_{circuit_size}_{error_rate}.json", "r") as report:
+                last_values = json.load(report)
 
-            # "fc_" + circuit_type + "_" + str(circuit_size) + "_" + str(error_mod) + "_" + str(error_rate) + ".csv"
-            res = pd.read_csv(path + "fc_" + circuit_type + "_" + circuit_size + "_" + error_mod + "_" + str(error_rate) + ".csv")
-            icm_small = pd.read_csv(path + "icm_small_" + circuit_type + "_" + circuit_size + "_" + error_mod + "_" + str(error_rate) + ".csv")
+            """
+            The format of the JSON is the following:
+            
+                last_values["large_fc_failure_rate"][sample_i]
+                last_values["large_icm_failure_rate"][sample_i]
+                last_values["small_icm_failure_rate"][sample_i]
+                last_values["error_mod"][sample_i]
+    
+                # Compute averages
+                last_values["averages"]["large_fc_failure_rate"]
+                last_values["averages"]["large_icm_failure_rate"]
+                last_values["averages"]["small_icm_failure_rate"]
+                last_values["averages"]["error_mod"]
+            """
 
-            # uncomment below for flag and icm error plotted together
-            #plt.scatter(int(circuit_size), res, color='red')
-            #plt.scatter(int(circuit_size), icm_small, color='blue')
-            #plt.ylabel("logical error rate")
+            # In case one needs to iterate over
+            # for i in range(len(last_values)):
+
+            res = last_values["averages"]["large_fc_failure_rate"]
+            icm_small = last_values["averages"]["small_icm_failure_rate"]
+            error_mod = last_values["averages"]["error_mod"]
+
+            # uncomment below` for flag and icm error plotted together
+            plt.scatter(int(circuit_size), res, color='red')
+            plt.scatter(int(circuit_size), icm_small, color='blue')
+            plt.ylabel("logic`al error rate")
 
             # uncomment below for difference between flag and icm error
-            #plt.scatter(int(circuit_size), (res - icm_small), color='red')
-            #plt.ylabel("logical error rate difference")
-            
-            # uncomment below for flag error mod
-            #plt.scatter(int(circuit_size), float(error_mod), color='blue')
-            #plt.ylabel("flag error mod")
+            # plt.scatter(int(circuit_size), (res - icm_small), color='red')
+            # plt.ylabel("logical error rate difference")
 
-            # uncomment below for flag qubit amount
-            circuits_path = "circuits2/" # remember to pick correct path!
-            flag_circuit_path = circuits_path + "fc_" + circuit_type + "_" + str(circuit_size) + ".json"
-            flag_circuit: cirq.Circuit = cirq.read_json(flag_circuit_path)
-            fqs = list(filter(lambda q: 'f' in q.name, flag_circuit.all_qubits()))
-            n_of_fq = len(fqs)
-            plt.scatter(n_of_fq, float(error_mod))
-            plt.ylabel("flag error mod")
-            plt.xlabel("number of flag qubits, " + r'$\approx\sqrt{dq}$')
+            # uncomment below for flag error mod
+            # plt.scatter(int(circuit_size), float(error_mod), color='blue')
+            # plt.ylabel("flag error mod")
+
+            # # uncomment below for flag qubit amount
+            # import cirq
+            # circuits_path = "misc/circuits2/"  # remember to pick correct path!
+            # flag_circuit_path = circuits_path + "fc_" + circuit_type + "_" + str(circuit_size) + ".json"
+            # flag_circuit: cirq.Circuit = cirq.read_json(flag_circuit_path)
+            # fqs = list(filter(lambda q: 'f' in q.name, flag_circuit.all_qubits()))
+            # n_of_fq = len(fqs)
+            # plt.scatter(n_of_fq, float(error_mod))
+            # plt.ylabel("flag error mod")
+            # plt.xlabel("number of flag qubits, " + r'$\approx\sqrt{dq}$')
 
     plt.tight_layout()
-    #plt.xlabel("circuit size")
-    #plt.savefig("bigbudgetb123.png")
+    # plt.xlabel("circuit size")
+    # plt.savefig("bigbudgetb123.png")
 
-    plt.savefig("./plots/bigbdgtadderimprtmodfq.png")
+    # "./plots/bigbdgtadderimprtmodfq.png"
+    plt.savefig(plot_fname)
