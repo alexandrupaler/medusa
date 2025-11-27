b24163e02ccb151518b7c05129339be67f6b5a0a
oksanenkaroliina <karoliina.i.oksanen@gmail.com>
2025-11-27 15:23:10 +0000

    more cleaning

--- preparation/icm_converter.jl
+++ preparation/icm_converter.jl
@@ -1,13 +1,155 @@
 using Jabalizer
 using PythonCall
+using JSON
 
 cirq = pyimport("cirq");
 gates_to_decomp = ["T", "T^-1"];
 
+"""
+Parses cirq qubits to qubits in the format needed for icm.
+Helper function.
+"""
+function parse_cirq_qubits(cirq_qubits::Vector{Any})
+    output_qubits = Vector{Int}()
+    for qubit in cirq_qubits
+        if qubit["cirq_type"] == "LineQubit"
+            push!(output_qubits, qubit["x"])
+        else
+            throw(DomainError(qubit["cirq_type"], "Only LineQubits are supported right now."))
+        end
+    end
+    return output_qubits
+end
+
+"""
+Loads cirq circuit from json into a format compatible with icm.
+"""
+function load_circuit_from_cirq_json(file_name::String)
+    raw_circuit = JSON.parsefile(file_name)
+    # Sometimes cirq saves to circuit as json with "\n" characters which require escaping
+    if typeof(raw_circuit) == String
+        raw_circuit = JSON.parse(replace(raw_circuit, "\n" => ""))
+    end
+
+    circuit = Vector{Tuple{String,Vector{Int}}}()
+    for moment in raw_circuit["moments"]
+        for operation in moment["operations"]
+
+            if !haskey(operation, "gate")
+                # There are other types of gates such as PauliStrings which do not have in the json
+                # the gate attribute
+                throw(DomainError(cirq_gate_name, "Gate type not supported"))
+            end
+
+            cirq_gate_name = operation["gate"]["cirq_type"]
+            qubits = parse_cirq_qubits(operation["qubits"])
+            if cirq_gate_name == "ZPowGate" && operation["gate"]["exponent"] == 0.25
+                gate_name = "T"
+            elseif cirq_gate_name == "ZPowGate" && operation["gate"]["exponent"] == -0.25
+                gate_name = "T^-1"
+            elseif cirq_gate_name == "HPowGate" && operation["gate"]["exponent"] == 1.0
+                gate_name = "H"
+            elseif cirq_gate_name == "CXPowGate" && operation["gate"]["exponent"] == 1.0
+                gate_name = "CNOT"
+            else
+                throw(DomainError(cirq_gate_name, "Gate type not supported"))
+            end
+            push!(circuit, (gate_name, qubits))
+        end
+    end
+    return circuit
+end
+
+#icm_input = load_circuit_from_cirq_json("input_cirq_circuit.json")
+
+"""
+# icm_circuit is saved as as Vector{Tuple{String,Vector{String}}}() but Jabalizer.compile needs Vector{Tuple{String,Vector{Int64}}}()
+# we convert the type here
+new_icm_input = Vector{Tuple{String,Vector{Int64}}}()
+for k in 1:length(icm_input)
+    g = icm_input[k][1]
+    q = icm_input[k][2]
+    qn = Vector{Int64}()
+    for i in 1:length(q)
+        s = q[i]
+        n = parse(Int64, s)
+        push!(qn, n)
+    end
+    push!(new_icm_input, (g, qn))
+end 
+"""
+
+#(icm_circuit,_) = Jabalizer.compile(icm_input, 10, gates_to_decomp, universal=false)
+
+#
+# based on Jabalizer.save_circuit_to_cirq with a fix for the qubit names & order
+#
+
+function save_circuit_to_cirq_json(circuit, file_name::String)
+    cirq_dict = Dict()
+    cirq_dict["cirq_type"] = "Circuit"
+    cirq_dict["moments"] = []
+
+    for (gate, qubits) in circuit
+
+        moment = Dict()
+        moment["cirq_type"] = "Moment"
+        operation = Dict()
+        operation["cirq_type"] = "GateOperation"
+        cirq_gate = Dict()
+        if gate == "T"
+            cirq_gate["cirq_type"] = "ZPowGate"
+            cirq_gate["exponent"] = 0.25
+        elseif gate == "T^-1"
+            cirq_gate["cirq_type"] = "ZPowGate"
+            cirq_gate["exponent"] = -0.25
+        elseif gate == "H"
+            cirq_gate["cirq_type"] = "HPowGate"
+            cirq_gate["exponent"] = 1.0
+        elseif gate == "CNOT"
+            cirq_gate["cirq_type"] = "CXPowGate"
+            cirq_gate["exponent"] = 1.0
+        elseif gate == "CZ"
+            cirq_gate["cirq_type"] = "CZPowGate"
+            cirq_gate["exponent"] = 1.0
+        else
+            throw(DomainError(gate, "Gate type not supported"))
+        end
+        cirq_gate["global_shift"] = 0.0
+        cirq_qubits = []
+        for qubit in qubits
+            cirq_qubit = Dict()
+            cirq_qubit["cirq_type"] = "NamedQubit"
+            name = "9999999" * string(qubit)
+            #print(name)
+            #print("\n")
+            cirq_qubit["name"] =  name #the 0 is to make sure these come before the flags
+            push!(cirq_qubits, cirq_qubit)
+        end
+        operation["gate"] = cirq_gate
+        operation["qubits"] = cirq_qubits
+        moment["operations"] = [operation]
+        push!(cirq_dict["moments"], moment)
+    end
+    
+    json_string = JSON.json(cirq_dict)
+    open(file_name, "w") do f
+        write(f, json_string)
+    end
+
+end
+
+#save_circuit_to_cirq_json(icm_circuit,"test_out.json");
+
+#print("\n")
+#print("compilation done!\n")
+#print("\n")
+
 function icm_converter_ij(i, j)
-    icm_input = Jabalizer.load_circuit_from_cirq_json("input_cirq_circuit" * string(i) * string(j) * ".json")
+    gates_to_decomp = ["T", "T^-1"];
+    icm_input = load_circuit_from_cirq_json("input_cirq_circuit" * string(i) * string(j) * ".json")
     (icm_circuit,_) = Jabalizer.compile(icm_input, 10, gates_to_decomp, universal=false)
-    Jabalizer.save_circuit_to_cirq_json(icm_circuit,"output_cirq_icm_circuit" * string(i) * string(j) * ".json");
+    save_circuit_to_cirq_json(icm_circuit,"output_cirq_icm_circuit" * string(i) * string(j) * ".json");
 end
 
 a = parse(Int64, ARGS[1])
